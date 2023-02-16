1ad698dc59ad32132cdba7db1d35559583e44bd6
Aastha <saxena.aastha91@gmail.com>
2023-02-17 03:46:54 +0530

    passed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    answer=[]
+    if length>0:
+        answer.append(0)
+        
+        if length >1:
+            answer.append(1)
+        
+       
+            if length>2:
+                
+        
+        
+                
+        
+                for i in range(2,length):
+                    answer.append(answer[i-1]+answer[i-2])
+                    
+
+    print(answer)
+
+
+  
